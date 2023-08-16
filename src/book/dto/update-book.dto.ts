c37aa93a9be706b3788cf077d187a35ee09257d5
Riteek Rakesh <riteek7.12@gmail.com>
2023-08-17 02:52:11 +0530

    Pipes, Checking input vadilation and prd optional functionality in Inputs.

--- src/book/dto/update-book.dto.ts
+++ src/book/dto/update-book.dto.ts
@@ -1,10 +1,21 @@
+import { IsOptional, IsString, IsNumber, IsEnum } from "class-validator"
 import { Category } from "../schemas/book.schema"
 
 
 export class UpdateBookDto{
+    @IsOptional()
+    @IsString()
     readonly title: string
+    @IsOptional()
+    @IsString()
     readonly description: string
+    @IsOptional()
+    @IsString()
     readonly author: string
+    @IsOptional()
+    @IsNumber()
     readonly price: number
+    @IsOptional()
+    @IsEnum( Category, {message: `Please enter a catagory!`})
     readonly category: Category
 }
